a523081aa81aa46f4119126a25b928b1fdbade52
ljk <lijingke@huice.com>
2025-12-25 20:10:02 +0800

    fix: 测试pypi

--- jkexcel/__init__.py
+++ jkexcel/__init__.py
@@ -3,4 +3,3 @@ from .app.excel_type import ExcelType
 from .app.application_options import ExcelOptions
 from .workbook.workbook import Workbook
 from .workbook.sheet import Sheet
-from .version import __version__
